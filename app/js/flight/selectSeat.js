c480f9540a86184ef1a0f01a11b781d602657e9c
wojtekchm <wwchmiel933@gmail.com>
2020-08-29 11:38:50 +0200

    max selected seat < 9

--- app/js/flight/selectSeat.js
+++ app/js/flight/selectSeat.js
@@ -1,21 +1,56 @@
 import createSelectSeatInfo from "./createSelectSeatInfo";
 
+let seatCounter = 0;
+
 const selectSeat = (seat, selectedPlaces, planes, planeName) => {
-    seat.addEventListener("click", () => {
-        // Kolorowanie miejsca
-        seat.classList.toggle("active");
+  seat.addEventListener("click", () => {
+    // Pobieranie informacji na temat miejsca z svg
+    const row = seat.parentElement;
+    const rowId = row.getAttribute("id");
+    const className = row.parentElement.getAttribute("id");
+    const seatId = seat.getAttribute("id");
+    const seatNumber = seatId.slice(1);
+    const selectedPlacesWrap = document.getElementById("selectedPlaces");
 
-        // Pobieranie informacji na temat miejsca z svg
-        const row = seat.parentElement;
-        const rowId = row.getAttribute("id");
-        const className = row.parentElement.getAttribute("id");
-        const seatId = seat.getAttribute("id");
-        const seatNumber = seatId.slice(1);
+    const selectedSeatList = document.querySelectorAll(".placeWrap");
 
-        createSelectSeatInfo(row, rowId, className, seatId, seatNumber, selectedPlaces, planes, planeName);
+    let selectSeatFlag = false;
 
+    selectedSeatList.forEach((selectedSeat) => {
+      if (selectedSeat.getAttribute("id") === seatId) {
+        selectSeatFlag = true;
+      }
     });
 
-}
+    if (selectSeatFlag) {
+      const selectAgain = document.querySelector(`#selectedPlaces #${seatId}`);
+      selectedPlacesWrap.removeChild(selectAgain);
+      seat.classList.remove("active");
+      selectSeatFlag = false;
+      seatCounter--;
+    } else {
+      if (seatCounter < 9) {
+        seatCounter++;
 
-export default selectSeat
+        // Kolorowanie miejsca
+        seat.classList.add("active");
+
+        createSelectSeatInfo(
+          row,
+          rowId,
+          className,
+          seatId,
+          seatNumber,
+          selectedPlaces,
+          planes,
+          planeName
+        );
+      } else {
+        alert("Wybrano za dużo miejsc!");
+      }
+      console.log(seatCounter);
+    }
+  });
+};
+
+export default selectSeat;
